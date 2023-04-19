6af24acd1af9b1658ef7d811eb4ec6cb3fbb38fc
Brian Cox <45868083+BCI83@users.noreply.github.com>
2023-04-19 16:32:09 +0100

    Update profile_addition.sh

--- profile_addition.sh
+++ profile_addition.sh
@@ -1,12 +1,19 @@
+## Addition is from here down
 if [ -f /symphony/setup-config ]; then
-        ip=$(cat /symphony/setup-config | grep -oP '(?<=ip_address:).*')        # return everything after 'ip_address:' on the line it is found on in the setup-config file
-        ip="${ip// /}"                                                          # remove any/all spaces from the value
+        ip=$(cat /symphony/setup-config | grep -oP '(?<=ip_address:).*')
+        ip="${ip// /}"
+        nm=$(cat /symphony/setup-config | grep -oP '(?<=subnet_mask:).*')
+        nm="${nm// /}"
+        gw=$(cat /symphony/setup-config | grep -oP '(?<=default_gateway:).*')
+        gw="${gw// /}"
+        dns=$(cat /symphony/setup-config | grep -oP '(?<=dns1:).*')
+        dns="${dns// /}"
         aid=$(cat /symphony/setup-config | grep -oP '(?<=account_id:).*')
         aid="${aid// /}"
         dns1=$(cat /symphony/setup-config | grep -oP '(?<=dns1:).*')
         dns1="${dns1// /}"
         user=$(whoami)
-        if [ "$ip" = "" ]; then
+        if [ "$ip" = "" ] || [ "$nm" = "" ] || [ "$gw" = "" ] || [ "$dns" = "" ]; then
                 if [ "$user" = "root" ]; then
                         cd /symphony/
                         ./network-setup.sh
